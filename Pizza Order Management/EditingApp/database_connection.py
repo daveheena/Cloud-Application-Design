7940bc7d85a3ba167ca4170c06457731590d9736
= <=dave.heena05@gmail.com>
2018-03-23 18:20:04 -0400

    Deleting results view

--- Pizza Order Management/EditingApp/database_connection.py
+++ Pizza Order Management/EditingApp/database_connection.py
@@ -7,9 +7,6 @@ def connect():
         querystring = ""
         connection = mysql.connector.connect(user='admin', password='pizza123', host='165.227.55.19', database='pizzaordermanagement')
         cursor = connection.cursor()
-        cursor.execute("select * from orders")
-        result = cursor.fetchall()
-        print(result)
         return connection
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
