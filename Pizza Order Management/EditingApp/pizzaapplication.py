52e07edb78406d6c4a541e4d752e7282aa5ebd76
= <=dave.heena05@gmail.com>
2018-03-23 01:09:14 -0400

    Updating application connection

--- Pizza Order Management/EditingApp/pizzaapplication.py
+++ Pizza Order Management/EditingApp/pizzaapplication.py
@@ -1,13 +1,18 @@
 from database_connection import r
 import sys
 import warnings
+import redis
 
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore",category=DeprecationWarning)
     from bottle import get, post, run, debug, default_app, request, template, static_file 	
 
 if __name__ == '__main__':
-    channel = sys.argv[1]
+    r = redis.StrictRedis(
+        host='redis-13366.c11.us-east-1-3.ec2.cloud.redislabs.com',
+        port=13366,
+        password='Kdv3XhAGb1dZ0iJCE3NEdILsN5MIa2oV'
+    )
 
     pubsub = r.pubsub()
     pubsub.subscribe(channel)
